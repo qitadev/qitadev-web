0c0d6beedafbe527a7043d74cc832f4f32fc2084
Albarra Naufala <albarraerdanto@gmail.com>
2022-03-15 18:12:57 +0700

    fix faq section

--- components/LandingPageFaq.vue
+++ components/LandingPageFaq.vue
@@ -9,11 +9,11 @@
     </p>
     <ul class="mt-16 grid md:grid-cols-2 gap-6">
       <li class="border rounded-lg p-4">
-        <dt class="flex justify-between items-center">
+        <dt class="h-full flex justify-between items-center">
           <span class="font-medium">
             Apa langkah pertama yang dilakukan untuk menanyakan layanan?
           </span>
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
           </svg>
         </dt>
@@ -22,11 +22,11 @@
         </dd>
       </li>
       <li class="border rounded-lg p-4">
-        <dt class="flex justify-between items-center">
+        <dt class="h-full flex justify-between items-center">
           <span class="font-medium">
             Apakah layanan konsultasi dikenakan biaya?
           </span>
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
           </svg>
         </dt>
