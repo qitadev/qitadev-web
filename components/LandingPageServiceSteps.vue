4eb68133eaf383c846c4aa3348fcc0141df0e127
Albarra Naufala <albarraerdanto@gmail.com>
2022-03-15 09:38:58 +0700

    fix responsive in service steps section

--- components/LandingPageServiceSteps.vue
+++ components/LandingPageServiceSteps.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="mt-24 max-w-5xl mx-auto px-5 flex">
+  <section class="mt-24 max-w-5xl mx-auto px-5 flex flex-col md:flex-row">
     <img src="images/service-step-image.png" alt="Service Step" class="w-[460px]">
-    <div class="ml-8">
+    <div class="mt-8 md:mt-0 md:ml-8">
       <span class="block mb-2 font-medium">Alur Layanan</span>
       <h2 class="font-bold text-4xl text-primary">
         Kemudahan adalah prioritas kami
@@ -9,7 +9,7 @@
       <p class="mt-4 font-light">
         Cuplikan ringkas alur pembuatan produk digital bersama kami mulai dari awal sampai akhir tanpa perlu ribet.
       </p>
-      <ol class="mt-4 grid grid-cols-2 gap-6">
+      <ol class="mt-4 grid sm:grid-cols-2 gap-6">
         <li class="flex items-center">
           <span class="w-[64px] h-[64px] flex items-center justify-center rounded-full border-2 border-primary text-primary text-2xl font-medium">
             01
