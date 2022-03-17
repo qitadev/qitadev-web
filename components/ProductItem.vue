76cd929a8bd2d91001079e96fa030a96619c9a48
Reza Firdaus <mhmmdrezalif@gmail.com>
2022-03-17 14:17:41 +0700

    add products page and product-item component

---
+++ components/ProductItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="mt-16 max-w-5xl mx-auto px-5">
+    <div class="grid grid-cols-1 md:grid-cols-12 gap-6 mt-16">
+      <div class="col-span-1 md:col-span-6 flex flex-col justify-center">
+        <h1 class="text-4xl font-semibold">
+          Kunikahan.id
+        </h1>
+        <h2 class="text-xl font-medium mt-4">
+          Lorem Ipsum is simply dummy text of the printing
+        </h2>
+        <p class="mt-4">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+        </p>
+        <nuxt-link to="/" class="mt-4 hover:text-primary transition-colors">
+          Lihat Website
+          <svg xmlns="http://www.w3.org/2000/svg" class="inline h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
+          </svg>
+        </nuxt-link>
+      </div>
+      <div class="col-span-1 md:col-span-6 mt-8 md:mt-0 flex justify-center">
+        <img
+          src="images/kunikahkan-id.png"
+          alt=""
+          class="rounded-lg"
+        >
+      </div>
+    </div>
+  </section>
+</template>
--- pages/products.vue
+++ pages/products.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <!-- TODO -->
+  <div class="pb-24">
+    <section class="max-w-5xl mx-auto px-5">
+      <p class="mt-16 text-lg font-medium text-primary text-center">
+        Products
+      </p>
+      <h1 class="mt-2 font-semibold text-4xl text-center">
+        Karya kami untuk tumbuh kita bersama
+      </h1>
+      <p class="font-light mt-4 text-center">
+        Kami berupaya menghasilkan karya terbaik untuk membantu
+      </p>
+    </section>
+
+    <product-item />
+    <consultation-banner />
   </div>
 </template>
 
