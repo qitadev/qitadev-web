2e9711c108887caaf55223cfaef99bb7b2adb140
Albarra Naufala <albarraerdanto@gmail.com>
2022-03-19 18:28:09 +0700

    update about us page

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -5,69 +5,59 @@
         About Us
       </p>
       <h1 class="mt-2 font-semibold text-4xl text-center">
-        Cerita sukses kontribusi kami
+        Berkenalan lebih dalam dengan kami
       </h1>
       <p class="font-light mt-4 text-center">
-        Menjadi kebanggaan tak ternilai menjadi bagian dari proses tumbuh dan kembang bersama
+        Menurut kami, membangun kepercayaan terhadap sesama dimulai dari saling mengenal satu sama lain
       </p>
     </section>
 
     <section class="max-w-5xl mx-auto px-5 mt-16">
-      <div class="grid grid-cols-1 md:grid-cols-12 gap-8 mt-16">
-        <div class="col-span-1 md:col-span-6 flex flex-col justify-center">
-          <h1 class="text-4xl font-semibold">
-            History
-          </h1>
-          <h2 class="text-xl font-medium mt-4">
-            Lorem Ipsum is simply dummy text of the printing
-          </h2>
-          <p class="mt-4">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-          </p>
+      <div class="grid md:grid-cols-2 gap-6 mt-16">
+        <div class="flex items-center">
+          <div>
+            <h2 class="text-3xl font-medium">
+              Siapa kami?
+            </h2>
+            <p class="mt-4">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde aliquid blanditiis nesciunt mollitia ipsam officiis aliquam? Voluptatem, totam? Cumque, possimus. Ad fugit nam beatae unde, deleniti exercitationem ullam veniam repellat.
+            </p>
+          </div>
         </div>
-        <div class="col-span-1 md:col-span-6 mt-4 lg:mt-0 flex justify-center">
-          <img
-            src="images/history-image.png"
-            alt="history image"
-            class="rounded-lg"
-          >
-        </div>
+        <img
+          src="images/history-image.png"
+          alt="history image"
+          class="rounded-lg"
+        >
       </div>
     </section>
 
-    <section class="max-w-5xl mx-auto px-5 mt-16">
-      <div class="grid grid-cols-1 md:grid-cols-12 gap-8 mt-16">
-        <div class="vision col-span-1 md:col-span-6 flex flex-col justify-center border border-gray-400 hover:border-primary hover:text-primary transition-colors rounded-lg p-8">
-          <img
-            src="images/icon-vision.png"
-            alt="vision"
-            height="80px"
-            width="80px"
-          >
-          <h1 class="mt-4 font-medium text-4xl">
-            Vision
-          </h1>
-          <p class="mt-4">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-          </p>
-        </div>
-        <div class="mission col-span-1 md:col-span-6 flex flex-col justify-center border border-gray-400 hover:border-primary hover:text-primary transition-colors rounded-lg p-8">
-          <img
-            src="images/icon-mission.png"
-            alt="vision"
-            height="80px"
-            width="80px"
-          >
-          <h1 class="mt-4 font-medium text-4xl">
-            Mission
-          </h1>
-          <p class="mt-4">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-          </p>
-        </div>
+    <section class="max-w-5xl mx-auto px-5 mt-16 grid md:grid-cols-2 gap-6">
+      <div class="border rounded-lg p-6">
+        <img
+          src="images/icon-vision.png"
+          alt="vision"
+          class="w-16"
+        >
+        <h2 class="mt-4 font-medium text-2xl">
+          Visi
+        </h2>
+        <p class="mt-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam fuga provident et, hic quisquam laboriosam harum deleniti earum officiis eos magnam sunt aliquid, magni nemo. Dicta quos nihil velit corporis?
+        </p>
+      </div>
+      <div class="border rounded-lg p-6">
+        <img
+          src="images/icon-mission.png"
+          alt="mission"
+          class="w-16"
+        >
+        <h2 class="mt-4 font-medium text-2xl">
+          Misi
+        </h2>
+        <p class="mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eum officia consequuntur sed voluptatem quibusdam, pariatur delectus assumenda ipsam qui aliquam modi, earum non commodi eius animi nulla. Velit, quidem!
+        </p>
       </div>
     </section>
 
@@ -75,43 +65,46 @@
       <p class="text-center text-primary font-medium">
         Our Team
       </p>
-      <h1 class="text-center font-semibold text-4xl mt-4">
+      <h2 class="text-center font-semibold text-4xl mt-4">
         Persona dibalik layar qitadev
-      </h1>
-      <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-12">
-        <div class="flex flex-col items-center">
+      </h2>
+      <div class="max-w-2xl mx-auto flex flex-col sm:flex-row justify-center mt-16">
+        <div class="flex-1 text-center px-5 mb-5 sm:mb-0">
           <img
             src="images/albarra.png"
             alt="albarra naufala"
+            class="inline-block w-32 h-32 object-cover rounded"
           >
-          <h3 class="font-semibold text-center text-xl mt-4">
-            Albarra Naufala
+          <h3 class="font-semibold text-xl mt-4">
+            Albarra
           </h3>
-          <p class="text-gray-400">
+          <p class="text-gray-400 text-sm">
             Software Engineer
           </p>
         </div>
-        <div class="flex flex-col items-center">
+        <div class="flex-1 text-center px-5 mb-5 sm:mb-0">
           <img
             src="images/reza.png"
-            alt="reza firdaus"
+            alt="reza ali"
+            class="inline-block w-32 h-32 object-cover rounded"
           >
-          <h3 class="font-semibold text-center text-xl mt-4">
-            Reza Firdaus
+          <h3 class="font-semibold text-xl mt-4">
+            Reza
           </h3>
-          <p class="text-gray-400">
+          <p class="text-gray-400 text-sm">
             Software Engineer
           </p>
         </div>
-        <div class="flex flex-col items-center">
+        <div class="flex-1 text-center px-5 mb-5 sm:mb-0">
           <img
             src="images/farhan.png"
-            alt="muhammad farhan"
+            alt="farhan"
+            class="inline-block w-32 h-32 object-cover rounded"
           >
-          <h3 class="font-semibold text-center text-xl mt-4">
-            Muhammad Farhan
+          <h3 class="font-semibold text-xl mt-4">
+            Farhan
           </h3>
-          <p class="text-gray-400">
+          <p class="text-gray-400 text-sm">
             Product Designer
           </p>
         </div>
