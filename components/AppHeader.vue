ef8c6a017cc884e2134a261f1b1424143c7fd1cf
Reza Firdaus <mhmmdrezalif@gmail.com>
2022-03-19 16:54:29 +0700

    fix img src on AppHeader

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="p-5 bg-gray-50 flex justify-between items-center">
     <nuxt-link to="/">
-      <img src="images/qitadev-logo.svg" alt="Qitadev Logo" class="w-32">
+      <img src="~/static/images/qitadev-logo.svg" alt="Qitadev Logo" class="w-32">
     </nuxt-link>
     <button class="lg:hidden">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -11,23 +11,23 @@
     <nav class="hidden lg:block">
       <nuxt-link to="/services/" class="link mr-8 relative">
         Services
-        <img src="images/stripe-short.svg" alt="Stripe" class="stripe absolute w-0 inset-x-0 -bottom-3">
+        <img src="~/static/images/stripe-short.svg" alt="Stripe" class="stripe absolute w-0 inset-x-0 -bottom-3">
       </nuxt-link>
       <nuxt-link to="/works/" class="link mr-8 relative">
         Works
-        <img src="images/stripe-short.svg" alt="Stripe" class="stripe absolute w-0 inset-x-0 -bottom-3">
+        <img src="~/static/images/stripe-short.svg" alt="Stripe" class="stripe absolute w-0 inset-x-0 -bottom-3">
       </nuxt-link>
       <nuxt-link to="/about-us" class="link mr-8 relative">
         About Us
-        <img src="images/stripe-short.svg" alt="Stripe" class="stripe absolute w-0 inset-x-0 -bottom-3">
+        <img src="~/static/images/stripe-short.svg" alt="Stripe" class="stripe absolute w-0 inset-x-0 -bottom-3">
       </nuxt-link>
       <nuxt-link to="/products" class="link mr-8 relative">
         Products
-        <img src="images/stripe-short.svg" alt="Stripe" class="stripe absolute w-0 inset-x-0 -bottom-3">
+        <img src="~/static/images/stripe-short.svg" alt="Stripe" class="stripe absolute w-0 inset-x-0 -bottom-3">
       </nuxt-link>
       <nuxt-link to="/for-clients" class="link mr-8 relative">
         For Clients
-        <img src="images/stripe-short.svg" alt="Stripe" class="stripe absolute w-0 inset-x-0 -bottom-3">
+        <img src="~/static/images/stripe-short.svg" alt="Stripe" class="stripe absolute w-0 inset-x-0 -bottom-3">
       </nuxt-link>
       <nuxt-link to="/contact-us" class="rounded-lg border border-primary text-primary px-2 py-1 transition hover:bg-primary hover:text-white">
         Contact Us
