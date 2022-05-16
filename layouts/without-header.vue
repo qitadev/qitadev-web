f46fffe33008fc156222517ae8de39dcdbfc6433
Albarra Naufala <albarraerdanto@gmail.com>
2022-05-16 18:49:59 +0700

    update contact us page

---
+++ layouts/without-header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <div class="flex-1">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WithoutHeader'
+}
+</script>
