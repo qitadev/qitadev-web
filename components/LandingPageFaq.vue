23bd0bc83f4eb9bc3e09ebe733114cae7f5ded86
Albarra Naufala <albarraerdanto@gmail.com>
2022-05-22 22:14:37 +0700

    add faq

--- components/LandingPageFaq.vue
+++ components/LandingPageFaq.vue
@@ -7,33 +7,87 @@
     <p class="mt-4 font-light text-center">
       Beberapa kumpulan pertanyaan dan jawaban yang bisa membantu untuk mencapai tujuanmu
     </p>
-    <ul class="mt-16 grid md:grid-cols-2 gap-6">
-      <li class="border rounded-lg p-4">
-        <dt class="h-full flex justify-between items-center">
-          <span class="font-medium">
-            Apa langkah pertama yang dilakukan untuk menanyakan layanan?
-          </span>
-          <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-          </svg>
-        </dt>
-        <dd class="hidden">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus velit nostrum ut recusandae ullam consectetur error quidem voluptates enim sunt esse aut, porro laudantium possimus quasi ex nisi molestias nam?
-        </dd>
-      </li>
-      <li class="border rounded-lg p-4">
-        <dt class="h-full flex justify-between items-center">
-          <span class="font-medium">
-            Apakah layanan konsultasi dikenakan biaya?
-          </span>
-          <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-          </svg>
-        </dt>
-        <dd class="hidden">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus velit nostrum ut recusandae ullam consectetur error quidem voluptates enim sunt esse aut, porro laudantium possimus quasi ex nisi molestias nam?
-        </dd>
-      </li>
-    </ul>
+    <div class="mt-16 grid gap-6">
+      <details class="border rounded-lg bg-white">
+        <summary class="font-medium p-4 cursor-pointer">
+          Apa itu produk digital?
+        </summary>
+        <p class="p-4 border-t text-sm">
+          Layanan digital yang disediakan oleh Qitadev, yaitu pembuatan website, web application, dan UI/UX Design
+        </p>
+      </details>
+      <details class="border rounded-lg bg-white">
+        <summary class="font-medium p-4 cursor-pointer">
+          Apa langkah pertama yang dilakukan untuk menanyakan layanan pembuatan produk digital?
+        </summary>
+        <p class="p-4 border-t text-sm">
+          Halo, kamu bisa menghubungi untuk berkonsultasi bersama kami melalui call center via whatsapp, email, atau langsung menjadwal pertemuan melalui formulir yang telah disediakan.
+        </p>
+      </details>
+      <details class="border rounded-lg bg-white">
+        <summary class="font-medium p-4 cursor-pointer">
+          Apakah layanan konsultasi dikenakan biaya?
+        </summary>
+        <p class="p-4 border-t text-sm">
+          Tidak, layanan konsultasi Qitadev gratis. Langsung hubungi kami dan jadwalkan pertemuan dengan mengisi formulir yang telah disediakan.
+        </p>
+      </details>
+      <details class="border rounded-lg bg-white">
+        <summary class="font-medium p-4 cursor-pointer">
+          Apakah setelah berkonsultasi pembuatan produk digital akan dilanjutkan?
+        </summary>
+        <p class="p-4 border-t text-sm">
+          Kami akan memberikan penawaran kelanjutan dari request produk digital-mu.
+        </p>
+      </details>
+      <details class="border rounded-lg bg-white">
+        <summary class="font-medium p-4 cursor-pointer">
+          Berapa biaya pembuatan layanan digital dikenakan?
+        </summary>
+        <p class="p-4 border-t text-sm">
+          Menyesuaikan dengan kebutuhan dan keinginan pada produk digital, lalu akan dirincikan dalam bentuk dokumen penawaran.
+        </p>
+      </details>
+      <details class="border rounded-lg bg-white">
+        <summary class="font-medium p-4 cursor-pointer">
+          Bagaimana pengalaman Qitadev dalam mengerjakan proyek digital?
+        </summary>
+        <p class="p-4 border-t text-sm">
+          Kami sudah mengerjakan beberapa proyek digital yaitu website, web application, hingga UI/UX design dari berbagai industri, seperti unit bisnis, lembaga pendidikan, instansi-instansi dan lainnya.
+        </p>
+      </details>
+      <details class="border rounded-lg bg-white">
+        <summary class="font-medium p-4 cursor-pointer">
+          Apa perbedaan layanan pembuatan website dengan web application?
+        </summary>
+        <p class="p-4 border-t text-sm">
+          Layanan website memiliki keterbatasan untuk berinteraksi dengan pengunjung dan hanya memberikan informasi pada laman website. Layanan web application memiliki keluwesan dalam berinteraksi dengan pengunjung sehingga memiliki fitur pendukung untuk menjadikan laman website lebih informatif.
+        </p>
+      </details>
+      <details class="border rounded-lg bg-white">
+        <summary class="font-medium p-4 cursor-pointer">
+          Apakah pihak Qitadev bisa menyesuaikan kebutuhan dan permintaan tertentu pada produk layanan digital?
+        </summary>
+        <p class="p-4 border-t text-sm">
+          Sangat bisa, kami memprioritaskan kepuasan pelanggan sebagai layanan utama.
+        </p>
+      </details>
+      <details class="border rounded-lg bg-white">
+        <summary class="font-medium p-4 cursor-pointer">
+          Apakah layanan pembuatan produk digital akan dibantu sampai akhir oleh Qitadev?
+        </summary>
+        <p class="p-4 border-t text-sm">
+          Iya, tentunya menyesuaikan dengan alur pembuatan produk digital dan penawaran yang sudah disepakati di awal.
+        </p>
+      </details>
+      <details class="border rounded-lg bg-white">
+        <summary class="font-medium p-4 cursor-pointer">
+          Apa yang membuat proses pembuatan produk digital Qitadev berbeda?
+        </summary>
+        <p class="p-4 border-t text-sm">
+          Kami sangat adaptif dan kolaboratif, menyesuaikan kebutuhan kamu dengan membantu mengatur metrik,  membantu kami untuk pemahaman bisnis, hingga kami akan memberikan rancangan terperinci produk digital dengan anggaran yang jelas. Kalau kamu berkenan, kami siap membantu untuk menjadwalkan proses pengerjaan produk digital-mu hingga selesai.
+        </p>
+      </details>
+    </div>
   </section>
 </template>
